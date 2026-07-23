f2af9bc14c116654d13c3eda0c8bbc1ce63ad931
HPotch <maastanr@gmail.com>
2026-07-23 18:43:40 +0200

    Teeny tiny bugfix old to new input system

--- GMTK 26/Assets/Scripts/Prototype/GameManager.cs
+++ GMTK 26/Assets/Scripts/Prototype/GameManager.cs
@@ -16,6 +16,7 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R)) SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        if (UnityEngine.InputSystem.Keyboard.current.rKey.wasPressedThisFrame)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
