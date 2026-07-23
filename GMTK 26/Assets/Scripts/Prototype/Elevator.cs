29e3ef5e1ba21d61f9f3abfa6bf895489416ded9
HPotch <maastanr@gmail.com>
2026-07-23 09:58:39 +0200

    Elevators added
    With acceleration and velocity

---
+++ GMTK 26/Assets/Scripts/Prototype/Elevator.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public float Weight;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        Weight += TryGetWeight(other.gameObject);
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        Weight -= TryGetWeight(other.gameObject);
+    }
+
+    private float TryGetWeight(GameObject go)
+    {
+        return go.TryGetComponent<WeightObject>(out WeightObject weightObject) ? weightObject.Weight : 0f;
+    }
+}
---
+++ GMTK 26/Assets/Scripts/Prototype/Elevators.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Elevators : MonoBehaviour
+{
+    [SerializeField] private Elevator leftElevator;
+    [SerializeField] private Elevator rightElevator;
+    [SerializeField] private Vector2 heightRange;
+    [SerializeField] private float maxVelocity = 2f;
+    [SerializeField] private float accelerationForce = 0.1f;
+    [SerializeField] private float drag = 0.1f;
+    
+    private float _currentHeight = 0.5f;
+    private float _velocity = 0f;
+    
+    private void Update()
+    {
+        float weightDiff = rightElevator.Weight - leftElevator.Weight;
+        float acceleration = weightDiff * accelerationForce;
+        _velocity += acceleration * Time.deltaTime;
+        _velocity = Mathf.Clamp(_velocity, -maxVelocity, maxVelocity);
+        _currentHeight += _velocity * Time.deltaTime;
+
+        if (_currentHeight <= 0f) {
+            _currentHeight = 0f;
+            _velocity = 0f;
+        } else if (_currentHeight >= 1f) {
+            _currentHeight = 1f;
+            _velocity = 0f;
+        }
+
+        _velocity *= Mathf.Max(1f - drag * Time.deltaTime, 0f);
+        
+        _currentHeight = Mathf.Clamp01(_currentHeight);
+        
+        leftElevator.transform.position = new Vector3(
+            leftElevator.transform.position.x,
+            Mathf.Lerp(heightRange.x, heightRange.y, _currentHeight),
+            leftElevator.transform.position.z);
+        rightElevator.transform.position = new Vector3(
+            rightElevator.transform.position.x,
+            Mathf.Lerp(heightRange.x, heightRange.y, 1f - _currentHeight),
+            rightElevator.transform.position.z);
+    }
+}
