78d408d263874fd1e96d252eb10d557e48f4f2fd
HPotch <maastanr@gmail.com>
2026-07-23 14:10:34 +0200

    Dragging added

--- GMTK 26/Assets/Scripts/Prototype/Elevator.cs
+++ GMTK 26/Assets/Scripts/Prototype/Elevator.cs
@@ -1,21 +1,45 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Elevator : MonoBehaviour
 {
     public float Weight;
     
+    private Elevators _elevators;
+    private List<Rigidbody> _rigidbodies = new List<Rigidbody>();
+
+    private void Awake()
+    {
+        _elevators = transform.parent.GetComponent<Elevators>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+        if (_rigidbodies.Contains(other.attachedRigidbody)) return;
+        
         Weight += TryGetWeight(other.gameObject);
+        _rigidbodies.Add(other.attachedRigidbody);
     }
     
     private void OnTriggerExit(Collider other)
     {
+        if (!_rigidbodies.Contains(other.attachedRigidbody)) return;
+        
         Weight -= TryGetWeight(other.gameObject);
+        _rigidbodies.Remove(other.attachedRigidbody);
     }
 
     private float TryGetWeight(GameObject go)
     {
         return go.TryGetComponent<WeightObject>(out WeightObject weightObject) ? weightObject.Weight : 0f;
+    }
+
+    private void Update()
+    {
+        if (_elevators.GetVelocity() == 0f) return;
+        foreach (Rigidbody rb in _rigidbodies)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, _elevators.GetVelocity(), rb.linearVelocity.z);
+        }
     }
 }
--- GMTK 26/Assets/Scripts/Prototype/WeightObject.cs
+++ GMTK 26/Assets/Scripts/Prototype/WeightObject.cs
@@ -1,6 +1,12 @@
+using System;
 using UnityEngine;
 
 public class WeightObject : MonoBehaviour
 {
     public float Weight = 3f;
+
+    private void Awake()
+    {
+        transform.localScale = Vector3.one * Mathf.Pow(Weight, 0.5f) / 3f;
+    }
 }
